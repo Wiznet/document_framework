4472d0f33395b321deee4830db0624aaa8c61018
Albin-Joseph <60734163+albin-joseph-wiznet@users.noreply.github.com>
2020-08-12 23:12:49 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 //import clsx from 'clsx';
 import Layout from '@theme/Layout';
 //import Link from '@docusaurus/Link';
+//import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
