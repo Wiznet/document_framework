fef44af5884b26bcbc7c00c7546e45a5e26ba424
Albin-Joseph <60734163+albin-joseph-wiznet@users.noreply.github.com>
2020-08-25 12:43:01 +0530

    try

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,11 @@ import Product from '../../static/img/Products.png';
 import Design_guid from '../../static/img/Design Guide.png';
 import Var from '../../static/img/VAR.png';
 import '../css/home.css';
-
-function product_click()  { 
-    window.location ='docs/Product/products'     
-    };
-
-function Design_guid_click()  {
-    window.location = 'docs/Design-Guide/design_guide'
-   };
-
- function Var_click()  {
-    window.location ='docs/VAR-Products-using-WIZnet/var_products_using_wiznet'  
-   };
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import Link from '@docusaurus/Link';
 
 function Home() {
-
-  return (	 
+  return (	  
   <Layout>    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
                 <img src={BG} alt="Back ground image" className="bg_img"></img>
@@ -30,34 +19,40 @@ function Home() {
                     a place to find everything you require for WIZnet Products
                 </div>
             </div>
-
             <div className="row home_1 container-fluid main_div" align="center">
-
+               
                 <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-                    <div className="home1_div" onClick={product_click}>
+                <Link to={useBaseUrl('docs/Product/products')}> 
+                    <div className=" button home1_div">
+                     
                         <img
                             src={Product}
                             alt="Back ground image"
                             className="home1_img"></img>
                         <h3 className="text-center img_heading">Products</h3>
+                        
                     </div>
+                    </Link> 
                 </div>
-
+               
                 <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-                    <div className="home1_div" onClick={Design_guid_click}>
+                <Link to={useBaseUrl('docs/Design-Guide/design_guide')}> 
+                    <div className=" button home1_div">
                         <img
                             src={Design_guid}
                             alt="Back ground image"
                             className="home1_img"></img>
                         <h3 className="text-center img_heading">Design Guide</h3>
                     </div>
+                    </Link>
                 </div>
-
                 <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-                    <div className="home1_div" onClick={Var_click}>
+                <Link to={useBaseUrl('docs/VAR-Products-using-WIZnet/var_products_using_wiznet')}> 
+                    <div className=" button  home1_div">
                         <img src={Var} alt="Back ground image" className="home1_img"></img>
                         <h3 className="text-center img_heading">VAR Products</h3>
                     </div>
+                    </Link>
                 </div>
             </div>	
     </Layout>
