097e443c86f3dc5e37cd4a2febbc6259628513f8
Albin-Joseph <60734163+albin-joseph-wiznet@users.noreply.github.com>
2020-08-25 16:43:36 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ module.exports = {
       indexName: 'wiznet-cloud',
     },
     navbar: {
-      title: '',
+      title: 'WIZnet',
       logo: {
         alt: 'My Site Logo',
         src: 'img/wiznet_logo.svg',
